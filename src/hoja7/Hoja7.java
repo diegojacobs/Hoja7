58a5baaa188b97e75a78ff1cfaa2e664396775cf
diegojacobs <diegojacobs9595@gmail.com>
2014-09-07 20:35:38 -0600

    inicio

--- src/hoja7/Hoja7.java
+++ src/hoja7/Hoja7.java
@@ -5,10 +5,13 @@
  */
 
 package hoja7;
+import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  *
  * @author Diego Jacobs
+ * @author Roberto Chiroy
  */
 public class Hoja7 {
 
@@ -17,6 +20,26 @@ public class Hoja7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner _in  = new Scanner(System.in);
+        
+        String cadena;
+        System.out.println("Ingrese su mensaje: ");
+        cadena = _in.next();
+        String listaC = new String();
+        int[] listaF = new int[cadena.length()];
+        
+        for (int i=0;i<cadena.length();i++)
+        {
+            boolean band = false;
+            for (int j=0;j<listaC.length();j++)
+                if (listaC.substring(j, j).equals(cadena.substring(i, i)))
+                    listaF[j]++;
+                    band = true;
+            if (!band){
+                listaC=listaC+cadena.substring(i, i);
+                listaF[i]=1;
+            }
+        }
     }
     
 }
